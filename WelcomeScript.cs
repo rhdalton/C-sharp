1a592b942cfe303265299d737551f5de9145f01c
Robin D <rhdalton@gmail.com>
2018-11-17 13:46:25 -0800

    first commit

---
+++ WelcomeScript.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WelcomeScript
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remeber we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
