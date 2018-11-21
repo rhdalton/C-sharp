b1d8f3ca2d52b667afa81a18092c5298006729ac
Robin D <rhdalton@gmail.com>
2018-11-21 13:30:41 -0800

    add class dir and first class script

---
+++ class-examples/classes1.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
