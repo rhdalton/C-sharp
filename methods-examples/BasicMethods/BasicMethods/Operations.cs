4b86bf59ac41460f147b202fd53a479f9697b23b
Robin D <rhdalton@gmail.com>
2018-11-21 15:48:15 -0800

    updating solutions

---
+++ methods-examples/BasicMethods/BasicMethods/Operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicMethods
+{
+    public class Operations
+    {
+        public static int AddTen(int num)
+        {
+            return num + 10;
+        }
+        public static int MultiplyTen(int num)
+        {
+            return num * 10;
+        }
+        public static double DivideTen(int num)
+        {
+            return (double)num / 10;
+        }
+    }
+}
