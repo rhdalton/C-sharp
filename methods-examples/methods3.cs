02a29ff0c4931786cc66d8803e5bd84bb0cb03d2
Robin D <rhdalton@gmail.com>
2018-11-21 11:35:45 -0800

    add methods 3 script

---
+++ methods-examples/methods3.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace MathOperationClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            int num1 = 0, num2 = 99, result = 0;
+                  
+            while (!validInput)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the first number:");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("An integer is required.");
+                }
+            }
+
+            validInput = false;
+            while(!validInput)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the second number. You can skip this number by pressing enter without a value.");
+                    string input = Console.ReadLine();
+                    MathOperation op = new MathOperation();
+
+                    if (input == "")
+                    {
+                        result = op.OperationOne(num1);
+                    }
+                    else
+                    {
+                        num2 = Convert.ToInt32(input);
+                        result = op.OperationOne(num1, num2);
+                    }
+                    validInput = true;
+
+                    Console.WriteLine("The answer to {0} * {1} is {2}", num1, num2, result);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("An integer is required.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class MathOperation
+    {
+        public int OperationOne(int num1, int num2 = 99)
+        {
+            return num1 * num2;
+        }
+    }    
+}
