226c903172a026fe53f743a917982e1437a19f41
Robin D <rhdalton@gmail.com>
2018-11-21 15:53:25 -0800

    updating solutions

---
+++ methods-examples/MethodsExample1/MethodsExample1/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExample1
+{
+    public class Operations
+    {
+        public void OperationOne(int num1, int num2)
+        {
+            num1 = num1 + 10;
+
+            Console.WriteLine(num2);
+        }
+    }
+}
--- methods-examples/MethodsExample1/MethodsExample1/Program.cs
+++ methods-examples/MethodsExample1/MethodsExample1/Program.cs
@@ -6,23 +6,12 @@ namespace MethodsExample1
     {
         static void Main(string[] args)
         {
-            MathOperation op = new MathOperation();
+            Operations op = new Operations();
             op.OperationOne(10, 15);
 
             op.OperationOne(num1: 10, num2: 15);
 
-
             Console.ReadLine();
         }
-    }
-
-    public class MathOperation
-    {
-        public void OperationOne(int num1, int num2)
-        {
-            num1 = num1 + 10;
-
-            Console.WriteLine(num2);
-        }
-    }
+    }    
 }
