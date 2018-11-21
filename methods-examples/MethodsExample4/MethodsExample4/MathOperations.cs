83de7037a17cd2ca43f994f1042852b32261b502
Robin D <rhdalton@gmail.com>
2018-11-21 15:39:33 -0800

    updating solutions

---
+++ methods-examples/MethodsExample4/MethodsExample4/MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExample4
+{
+    public class MathOperations
+    {
+        public void DivideByTwo(int num, out int result_num)
+        {
+            result_num = num / 2;
+        }
+
+        public void MethodWithoutParams()
+        {
+
+        }
+
+        public void MethodWithoutParams(int overloaded)
+        {
+
+        }
+        public void MethodWithoutParams(int overloaded, string anotheroverload)
+        {
+
+        }
+    }
+}
---
+++ methods-examples/MethodsExample4/MethodsExample4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodsExample4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valid_input = false;
+
+            while (!valid_input)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number:");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    MathOperations op = new MathOperations();
+                    op.DivideByTwo(num, out int result_num);
+                    Console.WriteLine("{0} divided by 2 is {1}", num, result_num);
+                    valid_input = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter a whole number.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }    
+}
