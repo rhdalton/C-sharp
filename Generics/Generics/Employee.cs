22b8e575019805988cd6bb3df01ad196091d4d6d
Robin D <rhdalton@gmail.com>
2018-11-21 21:07:14 -0800

    add generics script

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> : Person//, IQuittable
+    {
+        public int Id { get; set; }
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Employee Info:");
+            base.SayName();
+        }
+
+        //public void GetEmployeeId()
+        //{
+        //    Console.WriteLine("ID: {0}", Id);
+        //}
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    return (employee1.Id == employee2.Id);
+        //}
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return (employee1.Id == employee2.Id);
+        //}
+        //public void Quit(Employee employee)
+        //{
+        //    Console.WriteLine("Employee: {0} {1} is quitting.", employee.FirstName, employee.LastName);
+        //}
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.things = new List<string>() { "A", "B", "C", "D", "E" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.things = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (string t in employee1.things)
+            {
+                Console.WriteLine(t);
+            }
+            foreach (int t in employee2.things)
+            {
+                Console.WriteLine(t);
+            }
+
+
+
+            //Employee employee1 = new Employee() { FirstName = "John", LastName = "Smith", Id = 10 };
+            //Employee employee2 = new Employee() { FirstName = "Bob", LastName = "Smith", Id = 10 };
+
+            //if (employee1 == employee2)
+            //{
+            //    Console.WriteLine("This is the same person.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This is not the same person.");
+            //} 
+            Console.ReadLine();
+        }
+    }
+}
