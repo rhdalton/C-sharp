57a602045744268b1b610eebbdbed82e71a307bd
Robin D <rhdalton@gmail.com>
2018-11-21 16:46:45 -0800

    add overload operator script

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public List<Person> EmployeeList { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Employee Info:");
+            base.SayName();
+        }
+
+        public void GetEmployeeId()
+        {
+            Console.WriteLine("ID: {0}", Id);
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return (employee1.Id == employee2.Id);
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return (employee1.Id == employee2.Id);
+        }
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("Employee: {0} {1} is quitting.", employee.FirstName, employee.LastName);
+        }
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OverloadOperator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Smith", Id = 10 };
+            Employee employee2 = new Employee() { FirstName = "Bob", LastName = "Smith", Id = 10 };
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("This is the same person.");
+            }
+            else
+            {
+                Console.WriteLine("This is not the same person.");
+            } 
+            Console.ReadLine();
+        }
+    }
+}
