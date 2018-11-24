5fd5e7db6907f855e61af3bdcf1fd0d1c20b45be
Robin D <rhdalton@gmail.com>
2018-11-24 13:33:50 -0800

    add simple file logging

---
+++ SimpleFileLogging/SimpleFileLogging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+
+namespace SimpleFileLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number log to file:");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Robin\log.txt", input);
+
+            Console.WriteLine("Reading from same file:");
+            string fetch = File.ReadAllText(@"C:\Users\RobinDT\log.txt");
+            Console.WriteLine(fetch);
+
+            Console.ReadLine();
+        }
+    }
+}
