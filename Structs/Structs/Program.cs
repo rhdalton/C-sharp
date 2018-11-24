38fe9ed992a00b76f7313d0eaebca193f69e363b
Robin D <rhdalton@gmail.com>
2018-11-23 16:05:00 -0800

    add reference vs value types script

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueType valueType = new ValueType();
+            valueType.Amount = 99;
+            ValueType valueType2 = valueType;
+            valueType2.Amount = 11;
+
+            /* valueType.Amount remains 99 */
+            Console.WriteLine(valueType.Amount);
+
+            ReferenceType refType = new ReferenceType();
+            refType.Amount = 99;
+            ReferenceType refType2 = refType;
+            refType2.Amount = 11;
+
+            /* refType.Amount changes to 11 */
+            Console.WriteLine(refType.Amount);
+
+            Console.ReadLine();
+        }
+    }
+
+    public struct ValueType
+    {
+        public decimal Amount { get; set; }
+    }
+
+    public class ReferenceType
+    {
+        public decimal Amount { get; set; }
+    }   
+}
