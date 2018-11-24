a0d0eb1c49e7e1f2cd84acfd8c3d3d7693f6ea33
Robin D <rhdalton@gmail.com>
2018-11-24 13:49:56 -0800

    add simple datetime script

---
+++ SimpleDateTime/SimpleDateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Right now it is {0}", now);
+            Console.WriteLine("Enter a number:");
+            try
+            {
+                double num = Convert.ToDouble(Console.ReadLine());                
+                Console.WriteLine("The time {0} hours from now: {1}", num, now.AddHours(num));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Need to enter valid number.");
+            }
+
+            Console.ReadLine();            
+        }
+    }
+}
