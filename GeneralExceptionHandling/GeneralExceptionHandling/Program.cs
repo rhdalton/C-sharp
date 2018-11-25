88ece55a8aebfa82ccb403f518bb49d2b8d2a23b
Robin D <rhdalton@gmail.com>
2018-11-24 16:32:46 -0800

    add general exception handling example

---
+++ GeneralExceptionHandling/GeneralExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GeneralExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            while (!validInput)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your age:");
+                    int age = Convert.ToInt32(Console.ReadLine());
+
+                    if (age < 1)
+                    {
+                        throw new OtherExceptions("Age can't be lower than 1.");
+                    }
+                    else
+                    {
+                        var dateNow = DateTime.Now;
+                        var dateBorn = dateNow.AddYears(-age);
+                        Console.WriteLine("You were born in {0}", dateBorn.Year);
+                        validInput = true;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("You did not enter a valid number with no decimals.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class OtherExceptions : Exception
+    {
+        public OtherExceptions() : base() { }
+        public OtherExceptions(string message) : base(message) { }
+    }
+}
